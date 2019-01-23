43f8da40413297595d7b04fedb5cae974184c929
diwas kunwar <diwaskunwar2@gmail.com>
2019-01-23 16:13:02 +0545

    first commit

---
+++ new.js
@@ -0,0 +1,18 @@
+
+// var userName ,a ,b, Answer;
+// userName = prompt("plese enter your userName","name" );
+// document.write("Hello "+ userName + "<br  />");
+// document.write(userName + " The adition for your number is "+ "<br  />")
+// a = prompt("plese enter your your first number a" , "any number");
+// b = prompt("plese enter your your second number b", "any number" );
+// var x=parseInt(a);
+// var y=parseInt(b);
+// Answer = (x + y);
+// document.write( a+"+"+ b +"=" + Answer  );
+// localStorage.setItem("Answer","null");
+
+// function getAns(){
+//     var getAns = localStorage.getItem("Answer")
+// let saved = Answer.push(getAns)
+// }   
+// console.log("saved")
